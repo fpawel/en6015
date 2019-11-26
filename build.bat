049fb4ca4d8e6fc803d04a5dc6ec11c73cdb9faf
binf1611@gmail.com <fpawel>
2019-11-26 13:22:38 +0300

    save

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 SET APP_DIR=build
 SET GOARCH=386
-SET PACKAGE_TO_BUILD=github.com/fpawel/seh3dak6015/cmd/6015
+SET PACKAGE_TO_BUILD=github.com/fpawel/en6015/cmd/6015
 go build -o %APP_DIR%\6015.exe %PACKAGE_TO_BUILD%
